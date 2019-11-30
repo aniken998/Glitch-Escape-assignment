5399b394c09829d4fe3381f9657501f76019fbc1
aniken998 <enzhou@ucsc.edu>
2019-11-30 10:36:18 -0800

    finish without win-lose condition

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,12 @@ public class PlayerController : MonoBehaviour
     CharacterController cc;
 
     public float speed = 6.0f;
-    
     public float gravity = 20.0f;
+
+    public GameObject map1;
+    public GameObject map2;
+
+    private bool isMap1 = true;
     private Vector3 moveDirection;
     // Start is called before the first frame update
     void Start()
@@ -27,5 +31,23 @@ public class PlayerController : MonoBehaviour
         }
         moveDirection.y -= gravity * Time.deltaTime;
         cc.Move(moveDirection * Time.deltaTime);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit other){
+        Debug.Log("collide success");
+        if (other.gameObject.CompareTag("MapButton"))
+        {
+            Debug.Log("collide success");
+            if(isMap1) {
+                map1.SetActive(false);
+                map2.SetActive(true);
+                isMap1 = false;
+            }
+            else {
+                map1.SetActive(true);
+                map2.SetActive(false);
+                isMap1 = true;
+            }
+        }
     }
 }
